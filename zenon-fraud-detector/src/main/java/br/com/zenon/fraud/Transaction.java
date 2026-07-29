ca304d497fa2fae29a05885188855a0187bd819f
Rodrigo Navarro Góes <35267536+rngoes@users.noreply.github.com>
2026-07-28 21:16:08 -0300

    Tratamento de erros

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
@@ -1,8 +1,18 @@
 package br.com.zenon.fraud;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record Transaction(int step, TransactionType type, BigDecimal amount, TransactionCustumer origin,
                           TransactionCustumer recipient, boolean isFraud, boolean isFlaggedFraud) {
+
+    public Transaction {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(amount);
+        Objects.requireNonNull(origin);
+        Objects.requireNonNull(recipient);
+        if (step <= 0) throw new IllegalArgumentException("O valor de step deve ser positivo: " + step);
+        if (amount.signum() < 0) throw new IllegalArgumentException("O valor de amount deve ser positivo ou zero: " + amount);
+    }
 
 }
