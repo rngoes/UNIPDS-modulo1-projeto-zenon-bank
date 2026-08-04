a146eb515a54c46b520a45adfd8e00a8057d2d8f
Rodrigo Navarro Góes <35267536+rngoes@users.noreply.github.com>
2026-08-03 21:59:26 -0300

    Relatorio otimizado com estatisticas

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
@@ -0,0 +1,17 @@
+package br.com.zenon.fraud;
+
+import br.com.zenon.fraud.TransactionReport.Statistics;
+
+public class ReportMain {
+
+    void main() {
+        var transactionReport = new TransactionReport();
+        Statistics statistics = transactionReport.generateReport("data/PS_20174392719_1491204439457_log.csv");
+        IO.println("""
+            Total de linhas: %d
+            Total de fraudes: %d
+            Valor total transacionado: %.2f
+            """.formatted(statistics.totalTransactions(), statistics.totalFrauds(), statistics.totalAmount()));
+    }
+
+}
