5237abb7f66244fe6b5f4abf4c479ca2e728031c
Rodrigo Navarro <rngoes@gmail.com>
2026-08-09 22:27:10 -0300

    Ingestão de dados para o MYSQL de 10.000 trasações

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/DBMain.java
@@ -0,0 +1,28 @@
+package br.com.zenon.fraud;
+
+import java.util.List;
+
+public class DBMain {
+
+    void main() {
+
+        ConnectionFactory.getConnection();
+        IO.println("Coneção com o BD criada.");
+
+        var repository = new TransactionSQLRepository();
+        repository.findByOriginName("C1231006815")
+                .ifPresentOrElse(IO::println,() -> IO.println("Transação não Encontrada"));
+
+        //var transactionIngestor = new TransactionIngestor();
+
+        //long statTimeSQL = System.nanoTime();
+        //List<Transaction> transactions = transactionIngestor.read("data/PS_20174392719_1491204439457_log.csv");
+        //IO.println(transactions.size());
+        //IO.println("Iniciando adição das trasações no BD...");
+        //transactions.forEach(repository::save);
+        //long endTimeSQL = System.nanoTime();
+        //IO.println("Tempo de insersão para list em ms: " + (endTimeSQL - statTimeSQL)/1_000_000.0);
+
+    }
+
+}
--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionRepository.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionRepository.java
@@ -3,5 +3,6 @@ package br.com.zenon.fraud;
 import java.util.Optional;
 
 public interface TransactionRepository {
+    void save(Transaction transaction);
     Optional<Transaction> findByOriginName(String originName);
 }
